984f97870ab07dc81f52a8ff061685414af9c5d4
deepBlueShark59 <108550319+deepBlueShark59@users.noreply.github.com>
2022-07-14 22:33:40 +0300

    Add files via upload

---
+++ led1.ino
@@ -0,0 +1,15 @@
+#define led1  5
+
+void setup()
+{
+pinMode(led1,OUTPUT);
+}
+
+void loop() {
+
+    digitalWrite(led1,HIGH);
+      delay(500);
+    digitalWrite(led1,LOW);
+      delay(500);
+
+}
